9666a47f0b1e8501e8ff93696fc4ac473af3ba48
Charlie Cheever <ccheever@gmail.com>
2022-02-04 12:24:38 -0800

    Adding colors from website
    In styles.js. Imported into App.js

--- wordle-app/App.tsx
+++ wordle-app/App.tsx
@@ -1,5 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+import { colors } from "./styles";
 
 export default function App() {
   return (
@@ -13,8 +15,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ wordle-app/styles.js
@@ -0,0 +1,13 @@
+export let colors = {
+  green: "#6aaa64",
+  darkenedGreen: "#538d4e",
+  yellow: "#c9b458",
+  darkenedYellow: "#b59f3b",
+  lightGray: "#d8d8d8",
+  gray: "#868888a",
+  darkGray: "#939598",
+  white: "#fff",
+  black: "#212121",
+  orange: "#f5793a",
+  blue: "#85c0f9",
+};
