a14d28e97c803d1c8baef4be187386f12bb83a76
Charlie Cheever <ccheever@gmail.com>
2022-02-04 12:32:21 -0800

    Loading fonts so they can be used

--- wordle-app/App.tsx
+++ wordle-app/App.tsx
@@ -2,11 +2,48 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 import { colors } from "./styles";
+import { useFonts } from "expo-font";
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    ClearSansRegular: require("./assets/fonts/clear-sans/ClearSans-Regular.ttf"),
+    ClearSansMedium: require("./assets/fonts/clear-sans/ClearSans-Medium.ttf"),
+    ClearSansMediumItalic: require("./assets/fonts/clear-sans/ClearSans-MediumItalic.ttf"),
+    ClearSansThin: require("./assets/fonts/clear-sans/ClearSans-Thin.ttf"),
+    ClearSansLight: require("./assets/fonts/clear-sans/ClearSans-Light.ttf"),
+    ClearSansItalic: require("./assets/fonts/clear-sans/ClearSans-Italic.ttf"),
+    ClearSansBold: require("./assets/fonts/clear-sans/ClearSans-Bold.ttf"),
+    ClearSansBoldItalic: require("./assets/fonts/clear-sans/ClearSans-BoldItalic.ttf"),
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text
+        style={{
+          fontFamily: "ClearSansRegular",
+          fontSize: 16,
+        }}
+      >
+        Clear Sans Regular
+      </Text>
+
+      <Text
+        style={{
+          fontFamily: "ClearSansMedium",
+          fontSize: 16,
+        }}
+      >
+        Clear Sans Medium
+      </Text>
+      <Text
+        style={{
+          fontFamily: "ClearSansBold",
+          fontSize: 16,
+        }}
+      >
+        Clear Sans Bold
+      </Text>
+
       <StatusBar style="auto" />
     </View>
   );
